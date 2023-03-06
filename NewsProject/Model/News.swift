0891028e77c594597093b7b0952ba8706aa054e9
Erbil Can Keleş <cankls2434@gmail.com>
2023-03-06 15:29:05 +0300

    MVVM ve API ile ilk proje

---
+++ NewsProject/Model/News.swift
@@ -0,0 +1,13 @@
+//
+//  News.swift
+//  NewsProject
+//
+//  Created by Erbil Can on 6.03.2023.
+//
+
+import Foundation
+
+struct News : Decodable{
+    let title: String
+    let story: String
+}
---
+++ NewsProject/Service/WebService.swift
@@ -0,0 +1,15 @@
+//
+//  WebService.swift
+//  NewsProject
+//
+//  Created by Erbil Can on 6.03.2023.
+//
+
+import Foundation
+
+class Webservice{
+    func haberleriIndir(url : URL, completion: @escaping ([News]?) -> ()){
+        
+        
+    }
+}

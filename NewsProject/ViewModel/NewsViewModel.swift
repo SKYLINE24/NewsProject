95b5ccc9cc6e47ce0962b8c1fb2972dcace46a2a
Erbil Can Keleş <cankls2434@gmail.com>
2023-03-12 13:42:26 +0300

    Extension kullanarak MVVM e daha uygun hale getirdim

--- NewsProject/ViewModel/NewsViewModel.swift
+++ NewsProject/ViewModel/NewsViewModel.swift
@@ -8,25 +8,25 @@
 import Foundation
 //GÖSTERİLECEK HABERLERİ HAZIRLIYORUZ BU MODEL DA 
 struct NewsTableViewModel{
-    
     let newList : [News]
-    
+}
+extension NewsTableViewModel{
     func numberOfRowsInSection() -> Int{
         return self.newList.count
         //kaç haber gelicekse on kadarlık bir index belirliyoruz
     }
-    
     func newAtIndexPath(_ index: Int) -> NewsViewModel{
         let news = self.newList[index]
         return NewsViewModel(news: news)//newslist içerinsende index olarak seçileni news a ata demiş olduk ve böylece diziden tekile döndürmeyi sağladık
-      
     }
 }
 
+
 struct NewsViewModel{
-    
     let news : News
     
+}
+extension NewsViewModel{
     var title : String{
         return self.news.title
     }
